d4a21aa0c4e58bc1f7ccbbe0057842a4b3797cc9
Carlos <ciglsias@gmail.com>
2021-05-10 13:49:19 -0400

    Dotnet core console to upload data direct to SQL

---
+++ UploadingAnuncios/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UploadingAnuncios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
