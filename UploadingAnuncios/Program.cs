673b1638318974197d87512727b203a0a37cfc4a
Carlos <ciglsias@gmail.com>
2021-05-10 14:22:22 -0400

    Full Processing

--- UploadingAnuncios/Program.cs
+++ UploadingAnuncios/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using RevApi.Models;
 
 namespace UploadingAnuncios
 {
@@ -6,7 +8,42 @@ namespace UploadingAnuncios
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Starting Uploading");
+
+            var inputFile = "input.txt";
+
+            var anuncios = AnuncioManual.Load(inputFile);
+
+            var context = new RevContext();
+
+            foreach (var anuncioManual in anuncios)
+            {
+                if (anuncioManual.Seccion > 10000 || anuncioManual.Categoria > 10000 || string.IsNullOrEmpty(anuncioManual.Titulo))
+                {
+                    continue;
+                }
+
+                var now = DateTime.Now;
+
+                var anuncioCMG = new AnuncioCMG()
+                {
+                    Titulo = anuncioManual.Titulo,
+                    Descripcion = anuncioManual.Texto,
+                    Telefono = anuncioManual.Telefono,
+                    Seccion = anuncioManual.Seccion,
+                    Categoria = anuncioManual.Categoria,
+                    Precio = 0,
+                    Creado = now,
+                    ModificadoPorUltimaVez = now,
+                    Visitas = 1,
+                };
+
+                context.AnunciosCMG.Add(anuncioCMG);
+            }
+
+            context.SaveChanges();
+
+            Console.WriteLine("Done");
         }
     }
 }
